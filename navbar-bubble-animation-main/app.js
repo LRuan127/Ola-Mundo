022f63a85a65be7bc87f3f38e2575fa05367f6b3
RuanLDL <franciscorsb707@gmail.com>
2024-04-24 17:24:24 -0300

    UPDAT

--- navbar-bubble-animation-main/app.js
+++ navbar-bubble-animation-main/app.js
@@ -2,8 +2,36 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
-
+document.querySelectorAll('.nav-list a').forEach(link => {
+    link.addEventListener('click', () => {
+      nav.classList.remove('active');
+    });
+  });
+  
 window.addEventListener("scroll", function(){
     let header = document.querySelector('.header'); // Corrigido para .header
     header.classList.toggle('rolagem', window.scrollY > 500);
 });
+
+document.querySelectorAll('.nav-list a').forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault(); // Evita o comportamento padrão do link
+  
+      const targetId = link.getAttribute('href'); // Obtém o ID do alvo do link
+      const targetSection = document.querySelector(targetId); // Obtém a seção de destino
+  
+      const headerHeight = document.querySelector('.header').offsetHeight; // Obtém a altura do cabeçalho
+  
+      if (targetSection) {
+        // Adicione um atraso de 500 milissegundos (0.5 segundos) antes de rolar suavemente
+        setTimeout(() => {
+          const targetPosition = targetSection.offsetTop - headerHeight; // Calcula a posição final da rolagem
+          window.scrollTo({ top: targetPosition, behavior: 'smooth' }); // Rolagem suave para a posição ajustada
+        }, 500);
+      }
+      
+      // Fecha o menu após clicar em um link (opcional)
+      nav.classList.remove('active');
+    });
+  });
+  
